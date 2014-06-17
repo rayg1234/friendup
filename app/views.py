abf2959748e94ce07df7d4401f9627fffea25d1a
rayg1234 <raym.gao@gmail.com>
2014-06-17 13:15:21 -0700

    test

--- app/views.py
+++ app/views.py
@@ -25,46 +25,59 @@ def index():
 	if form.validate_on_submit():
 		print "valid"
 		#get all other ints
-		interest1 = form.Interest1.data
-		topinterests = []
-		topinterests.append(form.Interest2.data)
-		topinterests.append(form.Interest3.data)
-		topinterests.append(form.Interest4.data)
-
-		#get the primary interest set with high fidelity
-		primary_intset = GetMatch.GenerateInterestSet([interest1],model1,0.60)
-		topintsets =[]
-		for i in topinterests:
-		    topintsets.append(GetMatch.GenerateInterestSet([i],model1,0.55))
-		intset_all = GetMatch.GenerateInterestSet(topinterests,model1,0.55)
 
 
-		#get subset of primary matches
-		primarymatches = GetMatch.MatchOnInterests(cur,primary_intset,limit=10000)
-		PIDS = [x[2] for x in primarymatches]
-		
-		#get refined matches on other matches
-		r = GetMatch.MatchOnInterests_subset(cur,intset_all,PIDS,limit=20)
-		
 
-		#start with r[0]
-
-		currentmatch = r[0]
-		matchphoto = GetMatch.GetPhoto_byPID(cur,currentmatch[0])
-		matchname = currentmatch[1]
-		matchset_top = GetMatch.GetIntersect(cur,primary_intset,currentmatch[0])
-		matchset_rest = []
-		#print currentmatch[0]
-		#print matchset_top
-		for curset in topintsets:
-			theintersect = GetMatch.GetIntersect(cur,curset,currentmatch[0])
-			matchset_rest.append(theintersect)
-			#print(theintersect)
-		#print matchset_rest
 		results = {'key':'value'}
 
 		return render_template('/index.html',form=form,results = results)
 	return render_template('/index.html', title ='Test',form = form)
+
+@app.route('/generate_match')
+def generate_match():
+    	primeint = request.args.get('primeint', 0, type=str)
+	int1 = request.args.get('int1', 0, type=str)
+	int2 = request.args.get('int2', 0, type=str)
+	int3 = request.args.get('int3', 0, type=str)
+
+	topinterests = []
+	topinterests.append(int1)
+	topinterests.append(int2)
+	topinterests.append(int3)
+
+	if(topinterests == []):
+		return "null"
+
+	#get the primary interest set with high fidelity
+	primary_intset = GetMatch.GenerateInterestSet([primeint],model1,0.60)
+	topintsets =[]
+	for i in topinterests:
+	    topintsets.append(GetMatch.GenerateInterestSet([i],model1,0.55))
+	intset_all = GetMatch.GenerateInterestSet(topinterests,model1,0.55)
+
+	#get subset of primary matches
+	primarymatches = GetMatch.MatchOnInterests(cur,primary_intset,limit=10000)
+	PIDS = [x[2] for x in primarymatches]
+	
+	#get refined matches on other matches
+	r = GetMatch.MatchOnInterests_subset(cur,intset_all,PIDS,limit=20)
+	
+	#start with r[0]
+	currentmatch = r[0]
+	matchphoto = GetMatch.GetPhoto_byPID(cur,currentmatch[0])
+	matchname = currentmatch[1]
+	matchset_top = list(GetMatch.GetIntersect(cur,primary_intset,currentmatch[0]))
+	matchset_rest = []
+	#print currentmatch[0]
+	#print matchset_top
+	for curset in topintsets:
+		theintersect = GetMatch.GetIntersect(cur,curset,currentmatch[0])
+		matchset_rest.append(list(theintersect))
+		#print(theintersect)
+	print matchset_rest
+
+	ret = {'photo':{'you':'yourphoto','match':matchphoto},'name':{'you':'yourname','match':matchname},'matchset_top':{'you':'yourset','match':matchset_top},'matchset_rest': {'you':'yourset','match':matchset_rest}}
+    	return jsonify(ret);
 
 @app.route('/_add_numbers')
 def add_numbers():
